e8b4a7634a8a654d4de703218353c8f4162f4826
BiaoWu <biaoliu_2007@163.com>
2016-11-03 14:37:13 +0800

    first draft of broadcast

---
+++ core/src/main/java/com/biao/broadcast/Broadcast.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 BiaoWu.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.biao.broadcast;
+
+import java.util.List;
+
+/**
+ * Dispatches events to listeners.
+ *
+ * @author biaowu.
+ */
+public class Broadcast {
+
+  private final Registry registry;
+  private final Dispatcher dispatcher;
+
+  Broadcast(Registry registry, Dispatcher dispatcher) {
+    this.registry = registry;
+    this.dispatcher = dispatcher;
+  }
+
+  /**
+   * Registers all methods that annotated by {@link Subscribe} to receive events.
+   */
+  public void register(Object listener) {
+    registry.register(listener);
+  }
+
+  /**
+   * Unregisters all methods that annotated by {@link Subscribe}.
+   */
+  public void unregister(Object listener) {
+    registry.unregister(listener);
+  }
+
+  /**
+   * Posts an event to all registered listeners.
+   */
+  public void post(Object event) {
+    List<Subscriber> eventSubscribers = registry.getSubscribers(event);
+    if (eventSubscribers.size() > 0) {
+      dispatcher.dispatch(event, eventSubscribers);
+    } else {
+      // TODO: 2016/11/3
+    }
+  }
+}
---
+++ core/src/test/java/com/biao/broadcast/BroadcastTest.java
@@ -0,0 +1,70 @@
+package com.biao.broadcast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author biaowu.
+ */
+public class BroadcastTest {
+
+  Broadcast broadcast;
+
+  @Mock Registry registry;
+  @Mock Dispatcher dispatcher;
+
+  @Before
+  public void setUp() throws Exception {
+    MockitoAnnotations.initMocks(this);
+
+    broadcast = new Broadcast(registry, dispatcher);
+  }
+
+  @Test
+  public void testRegister() throws Exception {
+    broadcast.register(this);
+    verify(registry).register(this);
+  }
+
+  @Test
+  public void testUnregister() throws Exception {
+    broadcast.unregister(this);
+    verify(registry).unregister(this);
+  }
+
+  @Test
+  public void testPostADeadEvent() throws Exception {
+    Object event = this;
+
+    List<Subscriber> subscribers = Collections.emptyList();
+    when(registry.getSubscribers(event)).thenReturn(subscribers);
+
+    broadcast.post(event);
+
+    verify(registry).getSubscribers(event);
+    verify(dispatcher, never()).dispatch(event, subscribers);
+  }
+
+  @Test
+  public void testPost() throws Exception {
+    Object event = this;
+
+    List<Subscriber> subscribers = new ArrayList<>();
+    subscribers.add(new Subscriber(null));
+    when(registry.getSubscribers(event)).thenReturn(subscribers);
+
+    broadcast.post(event);
+
+    verify(registry).getSubscribers(event);
+    verify(dispatcher).dispatch(event, subscribers);
+  }
+}
